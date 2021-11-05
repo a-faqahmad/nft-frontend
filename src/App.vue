ab61329c2de8fb5e7a3289d0f87959584a0a87cf
nushirvannaseer <nushirvannaseer@gmail.com>
2021-11-05 19:38:19 +0500

    Added list component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <List/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    List
   }
 }
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,68 @@
+<template>
+ 
+    <div id="list-container">
+      <li class="list-item" v-for="item in items" :key="item.title">
+        <p>{{ item.title }}</p>
+        <img class = "list-image" src="https://picsum.photos/200/300"/>
+        <p>{{item.content}}</p>
+      </li>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'List',
+  data() {
+    return {
+      items: [
+        { title: "Item 0", content: "Content item with description" },
+        { title: "Item 1", content: "Content item with description" },
+        { title: "Item 2", content: "Content item with description" },
+        { title: "Item 1", content: "Content item with description" },
+
+      ],
+    };
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.list-container{
+   display:flex;
+  padding-left:20vw;
+  padding-right: 20vw;
+  overflow: scroll; 
+
+}
+
+.list-image{
+  border-radius: 20px;
+  
+}
+
+.list-item{
+  background-color: black;
+  color: white;
+  min-height: 20vh;
+  width: 20%;
+    border-radius: 20px;
+}
+</style>
